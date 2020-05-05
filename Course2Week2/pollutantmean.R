256029989da52f27bf21a235bd53b0a732e173f6
Giulio Martena <giuliomariomartena@gmail.com>
2020-05-05 13:15:17 +0200

    Week 2 assignment - fixed NaN problem in nitrate col

--- Course2Week2/pollutantmean.R
+++ Course2Week2/pollutantmean.R
@@ -15,11 +15,17 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   for (i in id) {
     data <- read.csv(paste(nameconstructor(i),'.csv',sep=''))
     if (pollutant == 'sulfate') {
+      if (length(data$sulfate[!is.na(data$sulfate)]) == 0) {
+        next
+      }
       notNA <- data$sulfate[!is.na(data$sulfate)]
       meanNA <- meanNA + length(notNA)*mean(notNA, na.rm = TRUE)
       obsC <- obsC + length(notNA)
       weightedMean <- meanNA / obsC
     }  else if (pollutant == 'nitrate') {
+      if (length(data$nitrate[!is.na(data$nitrate)]) == 0) {
+        next
+      }
       notNA <- data$nitrate[!is.na(data$nitrate)]
       meanNA <- meanNA + length(notNA)*mean(notNA, na.rm = TRUE)
       obsC <- obsC + length(notNA)
